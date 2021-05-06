692829d90362bd9a2c415c9a912096cd226e4040
Hamza-ABAKI <hamza.abaki@gmail.com>
2021-05-06 17:33:22 +0100

    initial commit

---
+++ src/Components/Form.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Form, Col, Button } from "react-bootstrap";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+
+export const Forms = () => {
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridEmail" class="name">
+          <div>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter first name" />
+          </div>
+          <div>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter last name" />
+          </div>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group controlId="formGridAddress2">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control placeholder="+216 ..." />
+      </Form.Group>
+
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>State</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
